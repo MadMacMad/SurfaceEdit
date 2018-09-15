4fe98f9c2058a4452d2276ccc3f0f0ca133d2df0
Tilify <grenqiun@gmail.com>
2018-09-16 01:29:34 +0300

    Revert "Revert "INotifyPropertyChanged moved to new file. Filling layer class""
    This reverts commit 4d0274e256e80627afb78a710497acb537f86c41.

--- Assets/Scripts/Layer.cs
+++ Assets/Scripts/Layer.cs
@@ -10,6 +10,7 @@ namespace Tilify
 {
     public class Layer : ObjectChangedRegistrator
     {
+        public IReadOnlyList<ISurfaceAffector> SurfaceAffectors => surfaceAffectors.AsReadOnly();
         private List<ISurfaceAffector> surfaceAffectors;
 
         public Layer (UndoRedoRegister undoRedoRegister ) : base (undoRedoRegister)
@@ -17,8 +18,26 @@ namespace Tilify
 
         }
 
-        public void AddSurfaceAffector<T>(SurfaceAffector<T> affector) where T : TextureAffector
+        public void AddSurfaceAffector<T>(SurfaceAffector<T> surfaceAffector) where T : TextureAffector
         {
+            if (!surfaceAffectors.Contains(surfaceAffector))
+            {
+                surfaceAffector.OnNeedUpdate += OnSurfaceAffectorNeedUpdate;
+                surfaceAffectors.Add (surfaceAffector);
+            }
+            NotifyNeedUpdate ();
         }
+        public void RemoveSurfaceAffector<T> (SurfaceAffector<T> surfaceAffector) where T : TextureAffector
+        {
+            if ( surfaceAffectors.Contains (surfaceAffector) )
+            {
+                surfaceAffector.OnNeedUpdate -= OnSurfaceAffectorNeedUpdate;
+                surfaceAffectors.Remove (surfaceAffector as ISurfaceAffector);
+            }
+            NotifyNeedUpdate ();
+        }
+
+        private void OnSurfaceAffectorNeedUpdate(object sender)
+            => NotifyNeedUpdate ();
     }
 }
