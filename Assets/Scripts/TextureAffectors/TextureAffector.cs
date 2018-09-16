c497ec4925d45b7149cb17b5d6bd3b2346f8ab87
Tilify <grenqiun@gmail.com>
2018-09-16 23:46:30 +0300

    Add dispose

--- Assets/Scripts/TextureAffectors/TextureAffector.cs
+++ Assets/Scripts/TextureAffectors/TextureAffector.cs
@@ -9,10 +9,12 @@ using UnityEngine;
 
 namespace Tilify.TextureAffectors
 {
-    public abstract class TextureAffector : ObjectChangedRegistrator
+    public abstract class TextureAffector : ObjectChangedRegistrator, IDisposable
     {
         public TextureAffector(UndoRedoRegister undoRedoRegister) : base (undoRedoRegister) { }
 
         public abstract void Affect (RenderTexture texture);
+
+        public virtual void Dispose () { }
     }
 }
