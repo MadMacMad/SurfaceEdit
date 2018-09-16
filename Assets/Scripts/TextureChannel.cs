2cfa2c648896304a03c0c73d36bad13a219c3e0b
Tilify <grenqiun@gmail.com>
2018-09-16 19:30:46 +0300

    Added new user channels and mask channel. New [Flags] attribute

--- Assets/Scripts/TextureChannel.cs
+++ Assets/Scripts/TextureChannel.cs
@@ -1,12 +1,24 @@
-﻿namespace Tilify
+﻿using System;
+
+namespace Tilify
 {
+    [Flags]
     public enum TextureChannel
     {
-        Unknown,
-        Albedo,
-        Normal,
-        Roughness,
-        Metallic,
-        HeightDisplacement
+        Unknown     = 0,
+        Albedo      = 1,
+        Normal      = 2,
+        Roughness   = 4,
+        Metallic    = 8,
+        Height      = 16,
+        Mask        = 32,
+        User1       = 64,
+        User2       = 128,
+        User3       = 256,
+        User4       = 512,
+        User5       = 1024,
+        User6       = 2048,
+        User7       = 4096,
+        User8       = 8192
     }
 }
