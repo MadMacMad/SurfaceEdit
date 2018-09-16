e43a728d29c6ced0a6f712d1feaa5f0939a05857
Tilify <grenqiun@gmail.com>
2018-09-16 18:18:17 +0300

    Refactoring, Arg checks

--- Assets/Scripts/Surface.cs
+++ Assets/Scripts/Surface.cs
@@ -16,15 +16,21 @@ namespace Tilify
         private Dictionary<TextureChannel, RenderTexture> textures = new Dictionary<TextureChannel, RenderTexture> ();
 
         private Dictionary<TextureChannel, TextureProvider> providers;
-
-
+        
         public Surface (TextureChannel textureChannel, TextureProvider textureProvider, Vector2 worldSize)
-            : this(new Dictionary<TextureChannel, TextureProvider>{{ textureChannel, textureProvider }}, worldSize) { }
+            : this (new Dictionary<TextureChannel, TextureProvider> { { textureChannel, textureProvider } }, worldSize) { }
 
-        public Surface(Dictionary<TextureChannel, TextureProvider> textureProviders, Vector2 worldSize)
+        public Surface (Dictionary<TextureChannel, TextureProvider> textureProviders, Vector2 worldSize)
         {
             Assert.ArgumentNotNull (textureProviders, nameof (textureProviders));
             Assert.ArgumentTrue (textureProviders.Count >= 1, nameof (textureProviders) + ".Count is less then 1");
+
+            int index = 0;
+            foreach ( var pair in textureProviders )
+            {
+                Assert.ArgumentNotNull (pair.Value, $"{nameof (textureProviders)}[{index}]");
+                index++;
+            }
 
             WorldSize = TextureHelper.Instance.ClampWorldSize (worldSize);
 
@@ -33,24 +39,28 @@ namespace Tilify
             FillTexturesArray ();
         }
 
-        private void FillTexturesArray()
+        private void FillTexturesArray ()
         {
             foreach ( var pair in providers )
                 textures.Add (pair.Key, pair.Value.Provide ());
         }
-          
-        public Dictionary<TextureChannel, RenderTexture> SelectTextures(List<TextureChannel> selectionList)
+
+        public Dictionary<TextureChannel, RenderTexture> SelectTextures (List<TextureChannel> selectionList)
         {
+            Assert.ArgumentNotNull (selectionList, nameof(selectionList));
+
             return selectionList
                 .Where (s => textures.ContainsKey (s))
                 .ToDictionary (s => s, s => textures[s]);
         }
-        public void Reset(List<TextureChannel> selectionList)
+        public void Reset (List<TextureChannel> selectionList)
         {
-            foreach(var s in selectionList)
+            Assert.ArgumentNotNull (selectionList, nameof (selectionList));
+
+            foreach ( var s in selectionList )
                 providers[s].Override (textures[s]);
         }
-        public void ResetAll()
+        public void ResetAll ()
         {
             foreach ( var pair in textures )
                 providers[pair.Key].Override (textures[pair.Key]);
