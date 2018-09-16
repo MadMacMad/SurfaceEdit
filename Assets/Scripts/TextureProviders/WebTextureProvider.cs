b9993a461e6b4736433e334b5b83952a44cc4303
Tilify <grenqiun@gmail.com>
2018-09-17 01:14:08 +0300

    New class: WebTextureProvider
    downloads texture from link and gives it to you

---
+++ Assets/Scripts/TextureProviders/WebTextureProvider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Tilify.TextureProviders
+{
+    public class WebTextureProvider : TextureProvider
+    {
+        private string textureLink;
+
+        public WebTextureProvider (string textureLink, bool cacheTexture = true) : base (cacheTexture)
+        {
+            this.textureLink = textureLink;
+        }
+
+        protected override RenderTexture Provide_Internal ()
+        {
+            var client = new WebClient ();
+            var data = client.DownloadData (textureLink);
+            var texture = new Texture2D(2, 2);
+            texture.LoadImage (data);
+            var renderTexture = texture.ConvertToRenderTexture ();
+            Resources.UnloadUnusedAssets ();
+            return renderTexture;
+        }
+    }
+}
