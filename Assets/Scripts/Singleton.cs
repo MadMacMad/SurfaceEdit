7704f96a78de22235dc590a3ab847fda92591c4f
Tilify <grenqiun@gmail.com>
2018-09-16 18:02:20 +0300

    New Class: Singleton<T>

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tilify
+{
+    public abstract class Singleton<T> where T : class, new()
+    {
+        public static T Instance => instance.Value;
+        private static Lazy<T> instance = new Lazy<T> (() => new T ());
+    }
+}
