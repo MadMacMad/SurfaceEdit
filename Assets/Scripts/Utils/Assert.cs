8d4be0951c29b08bddce8a8b07f15d4defc5c3bf
Tilify <grenqiun@gmail.com>
2018-09-16 17:49:27 +0300

    New class: Assert

---
+++ Assets/Scripts/Utils/Assert.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tilify
+{
+    public static class Assert
+    {
+        public static void ArgumentTrue(bool value, string message)
+        {
+            if ( !value )
+                throw new ArgumentException (message);
+        }
+        public static void ArgumentNotNull<T>(string name, T obj) where T : class
+        {
+            if ( obj is null || obj == null )
+                throw new ArgumentNullException (name, name + " is null");
+        }
+    }
+}
