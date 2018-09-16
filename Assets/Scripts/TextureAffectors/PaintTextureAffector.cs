2ff31e80a7692d2a68f2d9e7e9e4b2d57c1cf4f6
Tilify <grenqiun@gmail.com>
2018-09-16 23:52:49 +0300

    New PaintTextureAffector class
    just template

---
+++ Assets/Scripts/TextureAffectors/PaintTextureAffector.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tilify.AffectorRenderer;
+using UnityEngine;
+
+namespace Tilify.TextureAffectors
+{
+    public class PaintTextureAffector : TextureAffector
+    {
+        private AffectorRendererStation rendererStation;
+
+        public PaintTextureAffector(UndoRedoRegister undoRedoRegister) : base (undoRedoRegister)
+        {
+            //rendererStation = new AffectorRendererStation ();
+        }
+
+        public override void Affect (RenderTexture texture)
+        {
+
+        }
+
+        public override void Dispose ()
+        {
+            //rendererStation.Dispose ();
+        }
+    }
+}
