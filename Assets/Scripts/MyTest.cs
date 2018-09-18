6f3016f94949287262b8ecb3a2a66ca451077b00
Tilify <grenqiun@gmail.com>
2018-09-18 22:44:06 +0300

    Update test

--- Assets/Scripts/MyTest.cs
+++ Assets/Scripts/MyTest.cs
@@ -16,8 +16,9 @@ namespace Tilify
 
         private void Start ()
         {
-            var surface = new Surface (new Dictionary<TextureChannel, TextureProvider> () { { TextureChannel.Albedo, new WebTextureProvider (linkToGitHubOctocat) } });
+            var surface = new Surface (new Dictionary<TextureChannel, TextureProvider> () { { TextureChannel.User1, new WebTextureProvider (linkToGitHubOctocat) } });
             var viz = new SurfaceVisualizer (UndoRedoRegister.Instance, surface, Vector2.one, SurfaceVisualizer.SurfaceRenderMode.Channel);
+            viz.RenderedChannel = TextureChannel.Normal;
         }
     }
 }
