0f657caf875fabfc383de822734e01bbf074136b
Tilify <grenqiun@gmail.com>
2018-09-19 00:28:16 +0300

    New class: LineSegmentDivider. Divides line segment into an array of points with a specified distance between them

---
+++ Assets/Scripts/Utils/LineSegmentDivider.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Tilify
+{
+    public static class LineSegmentDivider
+    {
+        public static Vector2[] DivideLineSegmentIntoPoints (Vector2 startPosition, Vector2 endPosition, float distanceBetweenPoints)
+        {
+            if ( startPosition == endPosition )
+                return new Vector2[] { startPosition };
+
+            if ( distanceBetweenPoints < .01f )
+                distanceBetweenPoints = .01f;
+
+            var originVector = endPosition - startPosition;
+            var normalizedVector = originVector.normalized;
+            var pointOffset = normalizedVector * distanceBetweenPoints;
+
+            var pointsCount = Mathf.FloorToInt(originVector.magnitude / distanceBetweenPoints);
+            var result = new Vector2[pointsCount];
+
+            var offsetPosition = startPosition;
+
+            for(int i = 0; i < result.Length; i++ )
+            {
+                result[i] = offsetPosition;
+                offsetPosition += pointOffset;
+            }
+
+            return result;
+        }
+    }
+}
