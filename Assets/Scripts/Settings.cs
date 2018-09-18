acb9545a2a981fc9c54fdc732e0cc909f563ce62
Tilify <grenqiun@gmail.com>
2018-09-18 23:12:09 +0300

    All fields made readonly. Added affectorRendererStationLayerID

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -8,7 +8,8 @@ namespace Tilify
 {
     public static class Settings
     {
-        public static Vector2Int minTextureSize = new Vector2Int(64, 64);
-        public static Vector2Int maxTextureSize = new Vector2Int(8192, 8192);
+        public static readonly Vector2Int minTextureSize = new Vector2Int(64, 64);
+        public static readonly Vector2Int maxTextureSize = new Vector2Int(8192, 8192);
+        public static readonly int affectorRendererStationLayerID = LayerMask.NameToLayer ("AffectorRendererStation");
     }
 }
