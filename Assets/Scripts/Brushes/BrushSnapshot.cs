368b84d7252625bdd1d30294237d03828638ce71
Tilify <grenqiun@gmail.com>
2018-09-19 00:30:56 +0300

    New class: BrushSnapshot. Contains information about the brush at specific moment

---
+++ Assets/Scripts/Brushes/BrushSnapshot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Tilify.Brushes
+{
+    public sealed class BrushSnapshot
+    {
+        public readonly Vector2 percentageSize;
+        public readonly float intervals;
+
+        public readonly RenderTexture brushStamp;
+
+        public BrushSnapshot (Vector2 percentageSize, float intervals, RenderTexture brushStamp)
+        {
+            percentageSize.Clamp01 ();
+
+            this.percentageSize = percentageSize;
+            this.intervals = Mathf.Clamp (intervals, .01f, 10f);
+            this.brushStamp = brushStamp;
+        }
+
+    }
+}
