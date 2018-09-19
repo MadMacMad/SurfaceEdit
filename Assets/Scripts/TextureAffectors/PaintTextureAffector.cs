1be3c400691d218c77a17a8cea9c50e5834afaef
Tilify <grenqiun@gmail.com>
2018-09-19 03:50:17 +0300

    Bug fix

--- Assets/Scripts/TextureAffectors/PaintTextureAffector.cs
+++ Assets/Scripts/TextureAffectors/PaintTextureAffector.cs
@@ -3,23 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tilify.AffectorRenderer;
+using Tilify.Brushes;
 using UnityEngine;
 
 namespace Tilify.TextureAffectors
 {
     public class PaintTextureAffector : TextureAffector
     {
-        private AffectorRendererStation rendererStation;
+        private static readonly Mesh quadMesh;
+
+        static PaintTextureAffector()
+        {
+            quadMesh = MeshBuilder.BuildQuad (Vector2.one).ConvertToMesh ();
+        }
+
+        private RendererStation rendererStation;
 
         public PaintTextureAffector(UndoRedoRegister undoRedoRegister) : base (undoRedoRegister)
         {
-            rendererStation = new AffectorRendererStation (Settings.affectorRendererStationLayerID);
+            rendererStation = new RendererStation (Settings.affectorRendererStationLayerID);
+        }
+
+        public void Paint(PaintEntry paintEntry)
+        {
+            var material = paintEntry.brushSnapshot.brush.Material;
+                
+            foreach ( var position in paintEntry.BrushPositions )
+            {
+                var go = new GameObject ("Brush");
+                var renderer = go.AddComponent<MeshRenderer> ();
+
+                go.AddComponent<MeshFilter> ().mesh = quadMesh;
+                renderer.material = material;
+                go.transform.position = position;
+                go.transform.localScale = paintEntry.brushSnapshot.percentageSize;
+
+                rendererStation.UseIt (go);
+            }
         }
 
         public override void Affect (RenderTexture texture)
         {
-
+            rendererStation.Render (texture);
         }
 
         public override void Dispose ()
