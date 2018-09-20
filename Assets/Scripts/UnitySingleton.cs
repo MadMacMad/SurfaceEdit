fe9acb6d8d87092e1fd09ea23736fb799422e532
Tilify <grenqiun@gmail.com>
2018-09-20 13:17:12 +0300

    New class: UnitySingleton<T> :/

---
+++ Assets/Scripts/UnitySingleton.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Tilify
+{
+    public abstract class UnitySingleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        public static T Instance => instance.Value;
+        private static Lazy<T> instance = new Lazy<T> (() => new GameObject(nameof(T)).AddComponent<T>());
+    }
+}
