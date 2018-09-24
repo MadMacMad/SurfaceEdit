61587d993af90a2ec84417e17c2227c6b5a414bb
Tilify <grenqiun@gmail.com>
2018-09-24 03:47:30 +0300

    New TextureAffector event: NeedRenderEventHandler

--- Assets/Scripts/TextureAffectors/TextureAffector.cs
+++ Assets/Scripts/TextureAffectors/TextureAffector.cs
@@ -1,10 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace SurfaceEdit.TextureAffectors
 {
+    public class NeedRenderEventArgs : EventArgs
+    {
+        public readonly IReadOnlyCollection<Vector2Int> ChunkPositions;
+
+        public NeedRenderEventArgs (List<Vector2Int> chunkPositions)
+        {
+            ChunkPositions = chunkPositions.AsReadOnly ();
+        }
+    }
+
+    public delegate void NeedRenderEventHandler (object sender, EventArgs eventArgs);
+
+
     public abstract class TextureAffector : PropertyChangedRegistrator, IDisposable
     {
+        public event NeedRenderEventHandler NeedRender;
+
         public TextureAffector(UndoRedoRegister undoRedoRegister) : base (undoRedoRegister) { }
 
         public abstract void Affect (RenderTexture texture);
