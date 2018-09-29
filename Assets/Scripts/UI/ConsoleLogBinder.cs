7a098f0247ef20d24c339d3a2aa573824ff99c8e
Tilify <grenqiun@gmail.com>
2018-09-29 18:52:12 +0300

    UI improvements. Console output in ui

---
+++ Assets/Scripts/UI/ConsoleLogBinder.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using TMPro;
+
+namespace SurfaceEdit
+{
+    [RequireComponent(typeof(RectTransform))]
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class ConsoleLogBinder : MonoBehaviour
+    {
+        private TextMeshProUGUI text;
+
+        private void Start ()
+        {
+            text = GetComponent<TextMeshProUGUI> ();
+            Application.logMessageReceived += (logString, stackStace, logType) =>
+            {
+                var color = new Color32(200, 200, 200, 255);
+                if ( logType == LogType.Error || logType == LogType.Exception )
+                    color = new Color32(255, 0, 36, 255);
+                else if ( logType == LogType.Warning )
+                    color = new Color32 (255, 126, 0, 255);
+
+                text.text = $"<color=#{ColorUtility.ToHtmlStringRGBA(color).Substring(0, 6)}>{logString.Replace("\n", " ")}</color>";
+            };
+        }
+    }
+}
