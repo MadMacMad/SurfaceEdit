18584d716f7f5a552aa28c6f90274398cf296210
Tilify <grenqiun@gmail.com>
2018-10-02 21:38:28 +0300

    New class: Bootstrap. Will be used for program initialization

---
+++ Assets/Scripts/Bootstrap.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SurfaceEdit
+{
+    public class Bootstrap : MonoBehaviour
+    {
+        private UndoRedoRegister undoRedoRegister;
+
+        private void Start ()
+        {
+            undoRedoRegister = new UndoRedoRegister ();
+
+            var inputManager = new InputManager ();
+
+            var chain = new InputTriggerConflictChain (
+                new InputTriggerKeyCombination (undoRedoRegister.Undo)
+                    .WhenKeyPress (KeyCode.LeftControl)
+                    .WhenAnyKeyDown (KeyCode.Z, KeyCode.F),
+
+                new InputTriggerKeyCombination (undoRedoRegister.Redo)
+                    .WhenKeyPress (KeyCode.LeftControl)
+                    .WhenKeyPress (KeyCode.LeftShift)
+                    .WhenAnyKeyDown (KeyCode.Z, KeyCode.F));
+
+            inputManager.AddTrigger (chain);
+        }
+    }
+}
