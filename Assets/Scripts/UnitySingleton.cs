84bfca1b8d70947c1feaffb265ecc231c86945bf
Tilify <grenqiun@gmail.com>
2018-10-02 15:48:22 +0300

    Removed unnecessary namespace

--- Assets/Scripts/UnitySingleton.cs
+++ Assets/Scripts/UnitySingleton.cs
@@ -8,13 +8,13 @@ namespace SurfaceEdit
         public static T Instance => instance.Value;
         private static Lazy<T> instance = new Lazy<T> (() =>
         {
-            var gos = GameObject.FindObjectsOfType<T> ();
+            var gos = FindObjectsOfType<T> ();
             if ( gos.Length > 1 )
             {
                 Debug.LogError ($"There is more than one({gos.Length}) GameObjects with {typeof (T).FullName} singleton script attached." +
                                 $"All GameObjects, except one, will be destroyed.");
                 for ( int i = 1; i < gos.Length; i++ )
-                    GameObject.Destroy (gos[i]);
+                    Destroy (gos[i]);
                 return gos[0];
             }
             return new GameObject (typeof (T).Name).AddComponent<T> ();
