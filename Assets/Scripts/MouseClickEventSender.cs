f6ee00e5bdea825f34570e2ea4898da4e963c7b3
Tilify <grenqiun@gmail.com>
2018-10-04 12:10:04 +0300

    New class: MouseClickEventSender. Handles left, middle and right mouse clicks on ui

---
+++ Assets/Scripts/MouseClickEventSender.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+namespace SurfaceEdit
+{
+    // https://forum.unity.com/threads/can-the-ui-buttons-detect-a-right-mouse-click.279027/
+
+    public class MouseClickEventSender : MonoBehaviour, IPointerClickHandler
+    {
+        public UnityEvent OnLeftClicked;
+        public UnityEvent OnMiddleClicked;
+        public UnityEvent OnRightClicked;
+
+        public void OnPointerClick (PointerEventData eventData)
+        {
+            if ( eventData.button == PointerEventData.InputButton.Left )
+                OnLeftClicked?.Invoke ();
+            else if ( eventData.button == PointerEventData.InputButton.Middle )
+                OnMiddleClicked?.Invoke ();
+            else if ( eventData.button == PointerEventData.InputButton.Right )
+                OnRightClicked?.Invoke ();
+        }
+    }
+}
