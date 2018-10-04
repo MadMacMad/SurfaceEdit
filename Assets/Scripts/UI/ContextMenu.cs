c815c0d54c3becb9dbd74b1188ae0bd869f095a0
Tilify <grenqiun@gmail.com>
2018-10-04 12:08:34 +0300

    New class: ContextMenu. Destroys itself if loses focus

---
+++ Assets/Scripts/UI/ContextMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System;
+
+namespace SurfaceEdit
+{
+    public class ContextMenu : MonoBehaviour, IPointerExitHandler, IPointerEnterHandler
+    {
+        public event Action OnDestroy;
+
+        private bool hasFocus = false;
+
+        private void Update ()
+        {
+            if ( !hasFocus )
+                if ( Input.anyKeyDown )
+                {
+                    OnDestroy?.Invoke ();
+                    GameObject.Destroy (this);
+                }
+        }
+
+        public void OnPointerExit (PointerEventData eventData)
+            => hasFocus = false;
+        
+        public void OnPointerEnter (PointerEventData eventData)
+            => hasFocus = true;
+        
+    }
+}
