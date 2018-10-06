263011e4ff91cc1cfa26b8d722c662ecc15cdc46
Tilify <grenqiun@gmail.com>
2018-10-06 14:54:18 +0300

    Resource prefab. New class: ResourcePresenter

---
+++ Assets/Scripts/Presenters/ResourcesPresenter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurfaceEdit.Presenters
+{
+    public sealed class ResourcesPresenter
+    {
+        private ResourcesViewData viewData;
+
+        public ResourcesPresenter(ResourcesViewData viewData)
+        {
+            Assert.ArgumentNotNull (viewData, nameof (viewData));
+
+            this.viewData = viewData;
+        }
+    }
+}
